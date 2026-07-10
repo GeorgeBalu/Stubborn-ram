5079c8016df18b4fe02f0937ed4923818b29eaec
George Balu <egorbalyxin@mail.ru>
2026-07-10 16:12:48 +0500

    Prevent initial content flash on startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      setLoaded(true);
+    });
+  }, []);
+
   return (
-    <div className="app">
-
-  <div className="page">
-
-    <Hero />
-
-  </div>
-
-</div>
+    <div className={`app ${loaded ? "loaded" : ""}`}>
+      <div className="page">
+        <Hero />
+      </div>
+    </div>
   );
 }
 
